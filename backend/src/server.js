8875260d6370291fdabbbb5d766b5069fee66846
Raghunandan Shah <Raghunandanshah908014@gmail.com>
2025-12-03 22:49:39 +0530

    change in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,6 @@ import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
-const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
@@ -25,18 +24,10 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "api is up and running" });
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "server is running" });
 });
 
-// make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 const startServer = async () => {
   try {
